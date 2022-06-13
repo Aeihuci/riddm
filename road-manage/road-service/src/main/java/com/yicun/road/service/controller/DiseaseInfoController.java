c904d194e3f3bbf8d488f8d14bb4f6066347816b
gary-guo <1403098560@qq.com>
2022-06-13 16:55:28 +0800

    解决跨域问题

--- road-manage/road-service/src/main/java/com/yicun/road/service/controller/DiseaseInfoController.java
+++ road-manage/road-service/src/main/java/com/yicun/road/service/controller/DiseaseInfoController.java
@@ -1,16 +1,23 @@
 package com.yicun.road.service.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yicun.road.service.pojo.base.ResultCode;
+import com.yicun.road.service.pojo.base.RspPageInfo;
 import com.yicun.road.service.pojo.disease.TDefectsPits;
 import com.yicun.road.service.service.DiseaseService;
+import com.yicun.road.service.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 病害信息管理控制器
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/disease/")
+
 public class DiseaseInfoController {
 
     @Autowired
@@ -22,13 +29,13 @@ public class DiseaseInfoController {
      * @return
      */
     @PostMapping("queryDiseaseList")
-    public Page<TDefectsPits> queryTDefectsPitsList(@RequestBody TDefectsPits tDefectsPits){
+    public RspPageInfo<List<TDefectsPits>> queryTDefectsPitsList(@RequestBody TDefectsPits tDefectsPits){
         try {
             Page<TDefectsPits> page = diseaseService.queryTDefectsPitsList(tDefectsPits);
-            return page;
+            return ResponseUtil.makePageRsp(ResultCode.SUCCESS.CODE, page.getTotal(),"",page.getRecords());
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+            return ResponseUtil.makePageRsp(ResultCode.FAIL.CODE,0,"系统异常:" + e.getMessage(),null);
         }
     }
 
